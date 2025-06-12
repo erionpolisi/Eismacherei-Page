22cd5f88d51d75b0e2908160eb13eca9988c8673
erionpolisi <er.polisi@gmail.com>
2025-06-12 16:03:22 +0200

    haupt und optionales eis erste funktionierende version

--- assets/js/eis.js
+++ assets/js/eis.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const jsonUrl = `angebot-json/${id}.json`;
 
+
     try {
       const res = await fetch(jsonUrl);
       if (!res.ok) throw new Error(`Fehler beim Laden von ${jsonUrl}`);
@@ -17,7 +18,29 @@ document.addEventListener("DOMContentLoaded", async () => {
         eisDaten = eisDaten.filter(eis => eis.vegan);
       }
 
+      let optionalLabelInserted = false;
+      const newBr = () => document.createElement("br");
+
+      if(id != "hund"){
+      const mainLabel = document.createElement("div");
+          mainLabel.className = "label";
+          mainLabel.innerHTML = `<h3>Hauptsorten:</h3>`;
+          container.appendChild(mainLabel);
+          container.appendChild(newBr());
+      }
+
       eisDaten.forEach(eis => {
+        // Hier wird pro Sorte geprüft
+        if (!eis.hauptsortiment && !optionalLabelInserted && ( id != "hund" )) {
+          const optionalLabel = document.createElement("div");
+          optionalLabel.className = "label";
+          optionalLabel.innerHTML = `<h3>Optional:</h3>`;
+          container.appendChild(newBr());
+          container.appendChild(optionalLabel);
+          container.appendChild(newBr());
+          optionalLabelInserted = true;
+        }
+
         const div = document.createElement("div");
         div.className = "angebot-content";
         div.style.backgroundColor = eis.color;
@@ -34,13 +57,13 @@ document.addEventListener("DOMContentLoaded", async () => {
           </div>
         `;
 
-        // Flip-Effekt beim Klick
         div.addEventListener("click", () => {
           div.classList.toggle("flipped");
         });
 
         container.appendChild(div);
       });
+
     } catch (error) {
       console.error(`Fehler beim Laden der Eis-Daten für "${id}":`, error);
       alert("Fehler");
